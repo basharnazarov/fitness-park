ca334f7ca9243e5812e7949e2160e8184790be98
basharnazarov <bashar.odilovich@gmail.com>
2024-05-30 16:45:58 +0500

    added new tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { TabBarIcon, TabBarIcon2 } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -32,6 +32,33 @@ export default function TabLayout() {
           ),
         }}
       />
+        <Tabs.Screen
+            name="account"
+            options={{
+                title: 'Account',
+                tabBarIcon: ({ color, focused }) => (
+                    <TabBarIcon2 name={focused ? 'account' : 'account'} color={color} />
+                ),
+            }}
+        />
+        <Tabs.Screen
+            name="stats"
+            options={{
+                title: 'Stats',
+                tabBarIcon: ({ color, focused }) => (
+                    <TabBarIcon name={focused ? 'stats-chart' : 'stats-chart-outline'} color={color} />
+                ),
+            }}
+        />
+        <Tabs.Screen
+            name="news"
+            options={{
+                title: 'News',
+                tabBarIcon: ({ color, focused }) => (
+                    <TabBarIcon name={focused ? 'newspaper' : 'newspaper-outline'} color={color} />
+                ),
+            }}
+        />
     </Tabs>
   );
 }
