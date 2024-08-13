dd8edcbe94dda6ee7a51f1b87758a36eeb9c5655
basharnazarov <bashar.odilovich@gmail.com>
2024-08-13 14:08:39 +0500

    added some changes

---
+++ app/[index].tsx
@@ -0,0 +1,40 @@
+import { Image, StyleSheet, Platform, View, ScrollView } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Image
+                source={require('@/assets/images/partial-react-logo.png')}
+                style={styles.reactLogo}
+            />
+            <ThemedView style={styles.titleContainer}>
+                <ThemedText type="title">Welcome! starter page</ThemedText>
+            </ThemedView><HelloWave />
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+    },
+    stepContainer: {
+        gap: 8,
+        marginBottom: 8,
+    },
+    reactLogo: {
+        // height: "auto",
+        // width: 290,
+        objectFit: "cover",
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+    },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import {View, Text} from 'react-native';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -28,7 +28,11 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <View><Text>welcome</Text></View>
       <Stack>
+
+
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
